ec29b722ae75507e30d4dc1bec23a0591bc4b232
Amuthan Arulraj <arulraj.amuthan@gmail.com>
2017-11-20 02:43:56 -0500

    Adding sticky model

---
+++ api/models/sticky.js
@@ -0,0 +1,40 @@
+'use strict';
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+/**
+ * Mongoose schema for sticky object.
+ */
+let StickySchema = new Schema({
+    /**
+     * Title of the sticky.
+     */
+    title: {
+        type: String,
+        required: "title is required"
+    },
+    /**
+     * Sticky created date.
+     */
+    created_date: {
+        type: Date,
+        default: Date.now
+    },
+    /**
+     * Sticky content.
+     */
+    content: {
+        type: String
+    },
+    /**
+     * Last modified date.
+     */
+    modified_date: {
+        type: Date,
+        default: Date.now
+    }
+}, {
+    versionKey: false
+});
+
+module.exports = mongoose.model('Stickies', StickySchema);
