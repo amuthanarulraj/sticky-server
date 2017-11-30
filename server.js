97eba841cb5017fbd4cf447e9bc2abae995874a6
Amuthan Arulraj <arulraj.amuthan@gmail.com>
2017-11-30 18:58:25 -0500

    Adding CORS

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ let express = require('express'),
     bodyParser = require('body-parser');
 
 // mongoose instance connection url connection
-mongoose.connect('mongodb://localhost/stickyDB', { useMongoClient: true });
+mongoose.connect('mongodb://localhost/stickyDB', {
+    useMongoClient: true
+});
 mongoose.Promise = global.Promise;
 
 //Adding body parser for handling request and response objects.
@@ -13,6 +15,12 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 //Initialize app
 let initApp = require('./api/app');
